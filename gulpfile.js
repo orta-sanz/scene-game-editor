17f82a37604a1faff1e8c6aead45e2fb0b7874b8
WazaMonster <orta.sanz.93@gmail.com>
2015-05-20 18:34:15 +0200

    Gitignore, paquetes NPM iniciales y fichero Gulp iniciados

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+var gulp        = require('gulp'),
+	nib         = require('nib'),
+	notify      = require('gulp-notify'),
+	browserify  = require('gulp-browserify'),
+	stylus      = require('gulp-stylus');
+
+var watch = {
+	js  : './assets/**/*.js',
+	styl: './assets/**/*.styl'
+};
+
+var dest = {
+	js : './www/js',
+	css: './www/css'
+};
+
+gulp.task('scripts', function() {
+	return gulp.src('./assets/main.js')
+		.pipe(browserify({
+			insertGlobals: true,
+			transform: ['hbsfy']
+		}))
+		.pipe(gulp.dest(dest.js))
+		.pipe(notify({
+			onLast  : true,
+			title   : 'Scripts',
+			message : 'Generados'
+		}));
+});
+
+gulp.task('stylus', function() {
+	return gulp.src('./assets/main.styl')
+		.pipe(stylus({
+			use           : [nib()],
+			errors        : true,
+			'include css' : true
+		}))
+		.pipe(gulp.dest(dest.css))
+		.pipe(notify({
+			onLast   : true,
+			title    : 'Style',
+			message  : 'Generados'
+		}));
+});
+
+gulp.task('default', function() {
+	gulp.watch(watch.js, ['scripts']);
+	gulp.watch(watch.styl, ['stylus']);
+});
